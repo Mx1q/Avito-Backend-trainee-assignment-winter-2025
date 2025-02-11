9ab7da44935863da91cc6bebdcd12ef89cdc1c58
Mx1q <maxim.hal1tov@yandex.ru>
2025-02-11 11:33:11 +0300

    Jwt token init.

---
+++ internal/pkg/jwt/jwt.go
@@ -0,0 +1,38 @@
+package jwt
+
+import (
+	"fmt"
+	"github.com/golang-jwt/jwt/v5"
+	"time"
+)
+
+func createToken(username string, jwtKey string) (string, error) {
+	token := jwt.NewWithClaims(
+		jwt.SigningMethodHS256,
+		jwt.MapClaims{
+			"sub": username,
+			"iss": "AvitoShop",
+			"exp": time.Now().Add(time.Hour * 24).Unix(),
+			"iat": time.Now().Unix(),
+		})
+
+	tokenString, err := token.SignedString([]byte(jwtKey))
+	if err != nil {
+		return "", fmt.Errorf("signing token: %w", err)
+	}
+	return tokenString, nil
+}
+
+func verifyToken(tokenString string, jwtKey string) (*jwt.Token, error) {
+	token, err := jwt.Parse(tokenString, func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtKey), nil
+	})
+	if err != nil {
+		return nil, fmt.Errorf("parsing token: %w", err)
+	}
+	if !token.Valid {
+		return nil, fmt.Errorf("invalid token")
+	}
+
+	return token, nil
+}
