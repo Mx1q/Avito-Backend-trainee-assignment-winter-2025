5cf5a67dc829f385d5b0ad2b15dec1f1feddcd0c
Mx1q <maxim.hal1tov@yandex.ru>
2025-02-16 12:10:56 +0300

    Decreasing hash cost.

--- internal/pkg/jwt/hash.go
+++ internal/pkg/jwt/hash.go
@@ -5,6 +5,8 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
+const hashCost = bcrypt.MinCost
+
 type IHashCrypto interface {
 	HashPassword(password string) (string, error)
 	VerifyPassword(password, hash string) bool
@@ -18,7 +20,7 @@ func NewHashCrypto() IHashCrypto {
 }
 
 func (c HashCrypto) HashPassword(password string) (string, error) {
-	bytes, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), hashCost)
 	if err != nil {
 		return "", fmt.Errorf("generating hash: %w", err)
 	}
